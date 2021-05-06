5abf038842a910211322ec694820935520b713c5
Joseph Allen <joseph.allen@revature.net>
2021-05-06 18:15:42 -0500

    dashboard color stuff

--- src/main/java/reimaginedemployeemanager/Controllers/DashboardConfigController.java
+++ src/main/java/reimaginedemployeemanager/Controllers/DashboardConfigController.java
@@ -1,18 +1,18 @@
 package reimaginedemployeemanager.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-//import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import reimaginedemployeemanager.Objects.DashboardConfig;
 import reimaginedemployeemanager.Repositories.DashboardConfigRepository;
+import java.util.Optional;
 
 @CrossOrigin
 @RestController
@@ -30,4 +30,37 @@ public class DashboardConfigController {
         dcRepository.save(dc);
         System.out.println(dc.toString());
     }
+
+    @RequestMapping(value = "/getDashboardColor", 
+                    produces = MediaType.APPLICATION_JSON_VALUE, 
+                    method = RequestMethod.GET)
+    public ResponseEntity<Optional<DashboardConfig>>  getDashboardColor (DashboardConfig dc) {
+
+        return new ResponseEntity<>(dcRepository.findById(dc.getEmployeeID()), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/updateDashboardColor", 
+                    consumes = MediaType.APPLICATION_JSON_VALUE, 
+                    produces = MediaType.APPLICATION_JSON_VALUE, 
+                    method = RequestMethod.POST)
+    public void updateDashboardColor (@RequestBody DashboardConfig dc) {
+
+
+        Optional<DashboardConfig> updateDC = dcRepository.findById(dc.getEmployeeID());
+        
+        if (updateDC.isPresent()) {
+            
+           updateDC.get().setRed(dc.getRed());
+           updateDC.get().setGreen(dc.getGreen());
+           updateDC.get().setBlue(dc.getBlue());
+           dc = updateDC.get();
+           dcRepository.save(dc);
+        }
+
+
+    }
+
+
+
+
 }
